b5cbd3e6a5498659812be8bb5c28d862b377e26f
Guadalupe Vazquez <guvadlt@gmail.com>
2021-02-04 09:59:02 -0600

    Se agrego funcion

--- src/PruebaFunciones.scala
+++ src/PruebaFunciones.scala
@@ -1,7 +1,16 @@
 /*
+ * 1. Crea de forma ITERATIVA el programa que calcula la raíz cuadrada de un numero a través del algoritmo de Newton.
  * 
+ * El método computacional más común para calcular raíces cuadradas (y otras funciones también) 
+ * es el método de Newton de aproximaciones sucesivas. Cada vez que tenemos una estimación y del 
+ * valor de la raíz cuadrada de un número x, podemos hacer una pequeña manipulación para obtener una 
+ * mejor aproximación (una más cercana a la verdadera raíz cuadrada) promediando y con x/y.
  */
+import scala.math._
 
 object PruebaFunciones {
+  
+  def raizCuadrada(x:Int) : Int ={
+  }
   
 }
