01986591ff19d0972f5ad987b447fa503f5fa903
Guadalupe Vazquez <guvadlt@gmail.com>
2021-02-04 10:52:38 -0600

    Se agrego primera funcionalidad a la raiz cuadrada

--- src/PruebaFunciones.scala
+++ src/PruebaFunciones.scala
@@ -5,12 +5,23 @@
  * es el método de Newton de aproximaciones sucesivas. Cada vez que tenemos una estimación y del 
  * valor de la raíz cuadrada de un número x, podemos hacer una pequeña manipulación para obtener una 
  * mejor aproximación (una más cercana a la verdadera raíz cuadrada) promediando y con x/y.
+ * 
  */
 import scala.math._
 
 object PruebaFunciones {
+  var res=0.0
   
-  def raizCuadrada(x:Int) : Int ={
+  def raizCuadrada(x:Double, y:Double): Unit ={
+    var cuociente = x/y
+    var promedio= (cuociente+y)/2
+    
+    var estimacion=promedio*promedio
+    var dif=x-estimacion
+    if(estimacion<0.0001)
+      res=promedio
+    else
+      raizCuadrada(x,promedio) 
   }
   
 }
