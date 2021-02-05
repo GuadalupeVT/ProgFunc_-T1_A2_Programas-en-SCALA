86e5518206bb095beea9f13e8a29f4f809705d9a
Guadalupe Vazquez <guvadlt@gmail.com>
2021-02-05 14:08:37 -0600

    Se separo el proceso en funciones

--- src/PruebaFunciones.scala
+++ src/PruebaFunciones.scala
@@ -10,23 +10,43 @@
 import scala.math._
 
 object PruebaFunciones {
-  var res=0.0000
+  var res=0.0
   
-  def raizCuadrada(x:Double, y:Double): Unit ={
-    var cuociente = x/y
-    var promedio= (cuociente+y)/2
-    
+  
+  //Funcion para el cuociente
+  def cuociente(x:Double,y:Double): Double= {x/y}
+  
+  //Funcion para el promedio
+  def promedio(cuociente:Double,y:Double): Double={(cuociente+y)/2}
+  
+  //Funcion para calcular la salida
+  def dif(promedio:Double,x:Double): Boolean={
     var estimacion=promedio*promedio
     var dif=estimacion-x
     if(dif<0.0001)
-      res=promedio
+      true
     else
-      raizCuadrada(x,promedio) 
+      false
+  }
+     
+  //Calcular la raiz con el metodo de Newton
+  def raizCuadrada(x:Double, y:Double): Unit ={
+    var prom= promedio(cuociente(x,y),y)
+    if(dif(prom, x))
+      res=prom
+    else
+      raizCuadrada(x,prom) 
   }
   
+  
+  //Mostrar uso
   def main(args: Array[String]): Unit = {
     raizCuadrada(2,1)
-    println("La raiz cuadrada es :"+res)
+    println("La raiz cuadrada de 2 es :"+res)
+    
+    res=0
+    raizCuadrada(89,9)
+    println("La raiz cuadrada de 89 es :"+res)
   }
   
 }
